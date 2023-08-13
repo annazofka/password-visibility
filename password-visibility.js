b8f68de81f1f7f3fe44b11dd67082568718e84fb
Kocia <ankowalska@yahoo.co.uk>
2023-08-13 23:14:17 +0100

    add 'new password' field toggle

--- password-visibility.js
+++ password-visibility.js
@@ -1,12 +1,31 @@
-const password = document.querySelector("#password");
+// Password visibility for more than one password field:
+
+const passwords = document.querySelectorAll('[type="password"]');
 console.log(password)
-const togglePassword = document.querySelector("#toggle-password");
+const togglePassword = document.querySelector('#toggle-password');
 console.log(togglePassword)
 
-togglePassword.addEventListener("click" , () => {
+for (let password of passwords) {
+    togglePassword.addEventListener('click' , () => {
+        if(togglePassword.checked) {
+            password.type = 'text';
+        } else {
+            password.type = 'password';
+        }
+    });
+}
+
+// Password visibility for just one password field:
+
+/* const password = document.querySelector('#password');
+console.log(password)
+const togglePassword = document.querySelector('#toggle-password');
+console.log(togglePassword)
+
+togglePassword.addEventListener('click' , () => {
     if(togglePassword.checked) {
-        password.type = "text";
+        password.type = 'text';
     } else {
-        password.type = "password"
+        password.type = 'password';
     }
-});
+}); */
